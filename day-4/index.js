9a57223f917bf88ebd0a839d1d6d4db75ab1a84a
Emma Karayiannis <emma.karayiannis@gmail.com>
2020-12-10 11:09:04 +0000

    Day 4 part 1

---
+++ day-4/index.js
@@ -0,0 +1,12 @@
+const data = require('./input');
+
+const passports = data
+  .split('\n\n') // remove 2 line gaps and wrap everything in an array
+  .map(a => a.replace(/\n/g, ' ')) // remove line breaks and add spaces between items
+  .map(b => b.split(' ')) // split each line into it's own array + split each item up within it.
+  .map(c => c.map(d => d.substring(0, 3))) // get the part before the :
+  .map(e => e.filter(f => f != 'cid')) // strip out the cids in the arrays
+  .filter(g => g.length > 6) // we only want arrays with length of 7
+  .length;
+
+console.log(passports);
